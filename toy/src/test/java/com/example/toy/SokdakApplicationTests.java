4c37cef72fdc5296f07a79f0a4deb4e701c095f1
hyunrrr <whgusrms96@gmail.com>
2022-07-17 18:40:10 +0900

    chore: 초기 프로젝트 세팅

---
+++ toy/src/test/java/com/example/toy/SokdakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SokdakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
